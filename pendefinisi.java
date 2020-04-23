2ceb130242ee4c0f2f864650c1df32d7337d1f6d
Peggy25 <64076424+Peggy25@users.noreply.github.com>
2020-04-23 10:15:48 +0700

    Update pendefinisi.java

--- pendefinisi.java
+++ pendefinisi.java
@@ -1,2 +1,15 @@
-//pa
+//file untuk pendefinisi method
+public class pendefinisi {
+        public void menu(){
+        System.out.println("Selamat datang di program kalkulator luas bangun datar");
+        System.out.println("Silahkan pilih bangun datar ");
+        System.out.println("======================================================");
+        System.out.println("1.Persegi panjang");
+        System.out.println("2.Lingkaran");
+        System.out.println("3.Segitiga");
+        System.out.println("======================================================");
+        System.out.println("Pilih luas bangun datar 1-3 = ");
+    }
+
+}
 
